08f74879cf60c2bf1c11f2aef21f3e87b500f9a9
wiliki <wiliki@users.noreply.github.com>
2015-02-07 22:55:55 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+chrome.runtime.getBackgroundPage( function(bg) {
+    window.bg = bg
+
+    if (window.launchEntry) {
+        bg.haveentry(launchEntry)
+    } else if (bg.launchEntry) {
+        bg.haveentry(bg.launchEntry)
+    }
+})
+
+function filechange(evt) {
+    var file = evt.target.files[0]
+    bg.havefile(file)
+}
+document.getElementById('choosefile').addEventListener('change', filechange)
+
+function reload() { chrome.runtime.reload() }
